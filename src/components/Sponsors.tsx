5309cd5f04e4afe11399c263ef3e77153b9c3aad
Eric Soh <esoh0612@gmail.com>
2023-10-23 18:42:59 +0900

    chore: fix fetch image resource error

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,48 +1,37 @@
-import Image from 'next/image';
-
 const Sponsors = () => (
   <table className="border-collapse">
     <tbody>
       <tr className="h-56">
         <td className="border-2 border-gray-300 p-3">
           <a href="https://clerk.com?utm_source=github&utm_medium=sponsorship&utm_campaign=nextjs-boilerplate">
-            <Image
-              src="/assets/images/clerk.png"
+            <img
+              src="assets/images/clerk.png"
               alt="Clerk – Authentication & User Management for Next.js"
-              width={260}
-              height={224}
+              width="260"
             />
           </a>
         </td>
         <td className="border-2 border-gray-300 p-3">
           <a href="https://turso.tech/?utm_source=nextjsstarterbp">
-            <Image
+            <img
               src="/assets/images/turso.png"
               alt="SQLite Developer Experience"
-              width={260}
-              height={224}
+              width="260"
             />
           </a>
         </td>
         <td className="border-2 border-gray-300 p-3">
           <a href="https://upstash.com/?utm_source=nextjs-boilerplate">
-            <Image
-              src="/assets/images/redis.png"
-              alt="Upstash"
-              width={260}
-              height={224}
-            />
+            <img src="/assets/images/redis.png" alt="Upstash" width="260" />
           </a>
         </td>
       </tr>
       <tr className="h-56">
         <td className="border-2 border-gray-300 p-3">
           <a href="https://nextlessjs.com">
-            <Image
+            <img
               src="/assets/images/nextlessjs.png"
               alt="React SaaS Boilerplate Next.js"
-              width={260}
-              height={224}
             />
           </a>
         </td>
